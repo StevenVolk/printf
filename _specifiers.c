445620c37760b64fe8f7edfee6b2c070d68f86d1
Adedimeji <ojerindeadedimeji@gmail.com>
2022-11-18 09:21:04 -0800

    _specifiers.c

---
+++ _specifiers.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_char - prints the char variable
+ * @list: what to be printed
+ * Return: Always 1 for a character
+ */
+int print_char(va_list list)
+{
+	_putchar(va_arg(list, int));
+	return (1);
+}
+
+/**
+ * print_string - prints a string
+ * @list: what to be printed
+ * Return: number of characters printed
+ */
+int print_string(va_list list)
+{
+	int i;
+	char *pri;
+
+	pri = va_arg(list, char *);
+	if (pri == NULL)
+		pri = "nill";
+	for (i = 0; i != '\0'; i++)
+		_putchar(pri[i]);
+	return (i);
+}
+
+/**
+ * print_percent - prints a percentage symbol
+ * @list: what to be printed
+ * Return: Always 1 for a percentage symbol
+ */
+int print_percent(__attribute__((unused)) va_list list)
+{
+	_putchar('%');
+	return (1);
+}

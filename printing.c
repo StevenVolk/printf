f2572cb3bfd6780557082fa4120ab57ff87e2711
Adedimeji <ojerindeadedimeji@gmail.com>
2022-11-18 13:27:56 -0800

    printing.c

--- printing.c
+++ printing.c
@@ -10,7 +10,7 @@ int printing(const char *format, op_t t[], va_list list)
 {
 	int i, j, _count, count = 0;
 
-	while (i = 0; format[i] != '\0'; i++)
+	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%')
 		{
@@ -19,22 +19,14 @@ int printing(const char *format, op_t t[], va_list list)
 				if (format[i + 1] == t[j].op[0])
 				{
 					_count = t[j].f(list);
-					if (_count == -1)
-						return (-1);
 					count += _count;
 					break;
 				}
 			}
-			if (t[j].op == NULL && format[i + 1] != ' ')
+			if (t[j].op == NULL && format[i + 1] != ' ' && format[i + 1] != '\0')
 			{
-				if (format[i + 1] != '\0')
-				{
-					_putchar(format[i]);
-					_putchar(format[i + 1]);
-					count += 2;
-				}
-				else
-					return (-1);
+				_putchar(format[i]), _putchar(format[i + 1]);
+				count += 2;
 			}
 		}
 		else

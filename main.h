bcf13c2c28aab4cbb62160a62e095e67f985466f
Adedimeji <ojerindeadedimeji@gmail.com>
2022-11-18 09:22:53 -0800

    main.h

--- main.h
+++ main.h
@@ -18,5 +18,8 @@ typedef struct op
 
 int _putchar(char c);
 int _printf(const char *format, ...);
+int print_char(va_list list);
+int print_string(va_list list);
+int print_percent(__attribute__((unused)) va_list list);
 
 #endif /* MAIN_H */
